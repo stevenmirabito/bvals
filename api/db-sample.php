b4729b874d96a12f79e6760448a769773bf79b24
Henry Saniuk <henry@henrysaniuk.com>
2015-10-11 22:49:56 -0400

    Added db.php sample file and gitingore to ignore db.php

---
+++ api/db-sample.php
@@ -0,0 +1,19 @@
+<?php
+//Enter in your server variables and then rename this file as db.php
+    $host = "localhost";
+	$dbname = ""; //
+    $dbusername = ""; 
+    $dbpassword = "";
+    $membersTable = "members";
+    $queueTable = "queue";
+    $rosterTable = "roster";
+    $majorProjectsTable = "major_project";
+    $springEvalsTable = "spring_evals";
+    $houseMeetingsTable = "house_meetings";
+    $freshmanEvalsTable = "freshman_evals";
+    $conditionalTable = "conditionals";
+    $attendanceTable = "attendance";
+    $committeesTable = "committees";
+    $roomCurrent = "current";
+    $roomNext = "next";
+?>
